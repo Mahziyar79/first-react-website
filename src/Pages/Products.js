b43107c2097606b1e0ed92c38afa11aee0fe96ef
mahziyarGlp <mgillanpour79@gmail.com>
2021-10-07 16:27:41 +0330

    react select filter on product page added

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,10 +1,18 @@
 import { useEffect } from "react";
 import AllProductsSearch from "../components/Products/AllProductsSearch";
 import { useState } from "react";
+import Select from "react-select";
+
+const options = [
+  { value: "", label: "All" },
+  { value: "Travel", label: "Travel" },
+  { value: "Travel items", label: "Travel items" },
+  { value: "Equipment", label: "Equipment" },
+];
 
 const Products = () => {
   const [search, setSearch] = useState("");
-
+  const [selectOption, setSelectOption] = useState("");
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -13,18 +21,23 @@ const Products = () => {
   const changeInputSearchHandler = (e) => {
     setSearch(e.target.value);
   };
+  const selectInputSearchHandler = (e) => {
+    setSelectOption(e.value);
+  };
+
 
   return (
     <>
       <div className="search-product">
         <input
           type="text"
-          placeholder="search product..."
+          placeholder="Search on All Products..."
           value={search}
           onChange={changeInputSearchHandler}
         ></input>
+        <Select options={options} onChange={selectInputSearchHandler} />
       </div>
-      <AllProductsSearch search={search} />
+      <AllProductsSearch search={search} selectOption={selectOption}/>
     </>
   );
 };
--- src/components/Products/AllProductsSearch.js
+++ src/components/Products/AllProductsSearch.js
@@ -2,18 +2,34 @@ import Product from "./Product";
 import { products } from "../../data/products";
 import { useState, useEffect } from "react";
 
-const AllProductsSearch = ({ search }) => {
-  const [datas, setDatas] = useState(products);
+const AllProductsSearch = ({ search, selectOption }) => {
+  const [searchData, setSearchData] = useState(products);
+  const [selectData, setSelectData] = useState(products);
+  const [selectSupport, setSelectSupport] = useState(selectData);
+
+  useEffect(() => {
+    if (selectOption !== "") {
+      setSelectSupport(
+        products.filter((pro) =>
+          pro.category
+            .toLowerCase()
+            .includes(selectOption ? selectOption.toLowerCase() : null)
+        )
+      );
+    } else {
+      setSelectSupport(products);
+    }
+  }, [selectOption]);
 
   useEffect(() => {
     if (search !== "") {
-      setDatas(
-        products.filter((pro) =>
+      setSelectSupport(
+        selectData.filter((pro) =>
           pro.title.toLowerCase().includes(search ? search.toLowerCase() : null)
         )
       );
     } else {
-      setDatas(products);
+      setSelectSupport(selectSupport);
     }
   }, [search]);
 
@@ -24,8 +40,8 @@ const AllProductsSearch = ({ search }) => {
         <div className="divider"></div>
       </div>
       <div className="products">
-        {datas ? (
-          datas.map((product) => (
+        {selectSupport ? (
+          selectSupport.map((product) => (
             <div className="product" key={product.id}>
               <Product product={product} />
             </div>
